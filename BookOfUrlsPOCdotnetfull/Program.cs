4fe368bb7cf6e81496be4b1ab2995131b2e7551a
Simon Wu <siw@microsoft.com>
2016-12-23 10:11:35 +0800

    Add project files.

---
+++ BookOfUrlsPOCdotnetfull/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookOfUrlsPOCdotnetfull
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
